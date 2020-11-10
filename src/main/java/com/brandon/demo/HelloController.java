b79e391261fb11daf95a6041fd784fb6085a839c
Brandon Zhang <30281758+Brandon0204@users.noreply.github.com>
2020-11-10 23:20:24 +1300

    Update HelloController.java

--- src/main/java/com/brandon/demo/HelloController.java
+++ src/main/java/com/brandon/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Hello World spring boot. WIN";
+        return "I LOVE MY WIFE, JINGJING";
     }
 }
